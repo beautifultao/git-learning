b27d4f56381a19462f7e9a22ea0f26080e04aa8d
陈涛 <1147589357@qq.com>
2024-08-09 17:03:48 +0800

    第一次提交master

---
+++ src/main/java/com/cumt/atao/Main.java
@@ -0,0 +1,12 @@
+package com.cumt.atao;
+
+/**
+ * @DateTime: 2024/8/9
+ * @Description: TODO(一句话描述此类的作用)
+ * @Author: 阿涛
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/cumt/atao/master/v1.java
@@ -0,0 +1,9 @@
+package com.cumt.atao.master;
+
+/**
+ * @DateTime: 2024/8/9
+ * @Description: TODO(一句话描述此类的作用)
+ * @Author: 阿涛
+ **/
+public class v1 {
+}
